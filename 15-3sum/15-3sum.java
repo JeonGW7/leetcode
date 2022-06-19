f66268f5d9b5382b80dbeb51c58ff9ebb6edd951
JeonGW <86759846+JeonGW7@users.noreply.github.com>
2022-06-19 18:21:36 +0900

    Time: 41 ms (39.38%), Space: 58.4 MB (71.11%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); //배열 정렬(오름차순)
+        List<List<Integer>> list=new ArrayList();
+        for(int i1=0;i1+2<nums.length;i1++){
+            if(i1>0 && nums[i1]==nums[i1-1])
+                continue;
+            int i2=i1+1;
+            int i3=nums.length-1;
+            while(i2<i3){
+                int sum=nums[i1]+nums[i2]+nums[i3];
+                if(sum==0){
+                    List<Integer> tmp=new ArrayList();
+                    tmp.add(nums[i1]);
+                    tmp.add(nums[i2]);
+                    tmp.add(nums[i3]);
+                    list.add(tmp);
+                    i3--;
+                    while(i2<i3 && nums[i3]==nums[i3+1]){ //중복
+                        i3--;  
+                    }
+                }
+                else if(sum>0){
+                    i3--;
+                }else{ //sum이 0보다 작은경우 
+                    i2++;
+                }
+            }
+        }
+        return list;
+    }
+}
